966119e1aec3d8d2ac3ecbb2baf49443facd8948
sam caldwell <mail@samcaldwell.net>
2023-06-07 07:29:03 -0500

    adding bounds check to ConditionalCounter.IncrementIf()

--- ConditionalCounter.IncrementIf.go
+++ ConditionalCounter.IncrementIf.go
@@ -3,12 +3,12 @@ package counters
 // IncrementIf - increment if ok is true otherwise return -1
 func (counter *ConditionalCounter) IncrementIf(ok bool) int {
 	const notOK = -1 //If not ok, return -1
-	if ok {
-		//We increment after we return the value (if ok)
-		defer func() {
-			counter.value++
-		}()
-		return int(counter.value) //if ok, return state
-	}
+	defer func() {
+		if counter.value > uint(int(^uint(0)>>1)) {
+			panic("ConditionalCounter exceeds limit")
+		}
+		counter.value++
+
+	}()
 	return notOK
 }
