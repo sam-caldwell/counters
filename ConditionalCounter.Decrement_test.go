e8c4d536dc2c1311b0bb222afbb429dfe4b7385c
sam caldwell <mail@samcaldwell.net>
2023-06-06 19:20:32 -0500

    bug fix

--- ConditionalCounter.Decrement_test.go
+++ ConditionalCounter.Decrement_test.go
@@ -7,52 +7,34 @@ func TestConditionalCounter_Decrement(t *testing.T) {
 	if count.value != 0 {
 		t.Fail()
 	}
-	if count.IncrementIf(false) != -1 {
-		t.Fatal("expect -1")
+	if v := count.Increment(); v != 0 {
+		t.Fatalf("1.expect 0 Got:%d", v)
 	}
-	if count.IncrementIf(false) != -1 {
-		t.Fatal("expect -1")
+	if v := count.Increment(); v != 1 {
+		t.Fatalf("2.expect 1 Got:%d", v)
 	}
-	if count.IncrementIf(true) != 0 {
-		t.Fatal("expect 0")
+	if v := count.Increment(); v != 2 {
+		t.Fatalf("3.expect 2 Got:%d", v)
 	}
-	if count.IncrementIf(true) != 1 {
-		t.Fatal("expect 1")
+	if v := count.Increment(); v != 3 {
+		t.Fatalf("4.expect 3 Got:%d", v)
 	}
-	if count.IncrementIf(true) != 2 {
-		t.Fatal("expect 2")
+	if v := count.Decrement(); v != 4 {
+		t.Fatalf("5.expect 4 Got:%d", v)
 	}
-	if count.IncrementIf(false) != -1 {
-		t.Fatal("expect -1")
+	if v := count.Decrement(); v != 3 {
+		t.Fatalf("6.expect 3 Got:%d", v)
 	}
-	if count.value != 3 {
-		t.Fatal("final state expects 3")
+	if v := count.Decrement(); v != 2 {
+		t.Fatalf("7.expect 2 Got:%d", v)
 	}
-	if v := count.DecrementIf(false); v != -1 {
-		t.Fatalf("5.expect -1 Got:%d", v)
+	if v := count.Decrement(); v != 1 {
+		t.Fatalf("8.expect 1 Got:%d", v)
 	}
-	if count.value != 3 {
-		t.Fatal("final state expects 3")
+	if v := count.Decrement(); v != 0 {
+		t.Fatalf("9.expect 0 Got:%d", v)
 	}
-	if v := count.DecrementIf(false); v != -1 {
-		t.Fatalf("6.expect -1 Got:%d", v)
-	}
-	if count.value != 3 {
-		t.Fatal("final state expects 3")
-	}
-	if v := count.DecrementIf(true); v != 3 {
-		t.Fatalf("7.expect 3 Got:%d", v)
-	}
-	if v := count.DecrementIf(true); v != 2 {
-		t.Fatalf("8.expect 2 Got:%d", v)
-	}
-	if v := count.DecrementIf(true); v != 1 {
-		t.Fatalf("9.expect 1 Got:%d", v)
-	}
-	if v := count.DecrementIf(true); v != 0 {
-		t.Fatalf("10.expect 0 Got:%d", v)
-	}
-	if v := count.DecrementIf(true); v != 0 {
+	if v := count.Decrement(); v != 0 {
 		t.Fatalf("10.expect 0 Got:%d", v)
 	}
 }
